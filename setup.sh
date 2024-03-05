fbd3b4ce5d2a33552d11ae08713708503a06de43
TiagoRG <tiago.rgarcia@ua.pt>
2024-03-05 18:47:56 +0000

    Setup update
    Signed-off-by: TiagoRG <tiago.rgarcia@ua.pt>

--- setup.sh
+++ setup.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+# Save current directory
+ORIGIN_DIR=$(pwd)
+cd "$HOME" || exit
+
+# Install yay
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay || exit
+makepkg -si --noconfirm
+sudo pacman -U yay-*.pkg.tar.zst --noconfirm
+cd "$OLDPWD" || exit
+
 # Install the required packages
 # print in green
 echo -e "\e[32m$1\e[0mInstalling the required packages"
-sudo pacman -Sy stow vim neovim zsh git clang nodejs npm --noconfirm
+sudo yay -S stow vim neovim zsh git clang nodejs npm zoxide pfetch --noconfirm
 
 # Clone the dotfiles if it's not the current directory
 if [ "$(pwd)" != "$HOME/.dotfiles" ]; then
     echo -e "\e[32m$1\e[0mCloning the dotfiles repository"
     git clone "https://github.com/TiagoRG/.dotfiles.git" "$HOME/.dotfiles"
-    cd "$HOME/.dotfiles" || exit
 fi
 
 # Setup Vim
@@ -20,18 +30,16 @@ curl -fLo "$HOME/.dotfiles/.vim/autoload/onedark.vim" --create-dirs "https://raw
 echo -e "\e[32m$1\e[0mInstalling Copilot"
 git clone "https://github.com/github/copilot.vim.git" "$HOME/.dotfiles/.vim/pack/github/start/copilot.vim"
 echo -e "\e[32m$1\e[0mSetting up Copilot"
-stow -d "$HOME/.dotfiles" "$HOME"
+stow -d .dotfiles .
 /bin/vim -c 'Copilot setup' -c 'qa!'
 
 # Setup Neovim
 echo -e "\e[32m$1\e[0mSetting up Neovim"
 echo -e "\e[32m$1\e[0mInstalling Packer"
 git clone --depth 1 "https://github.com/wbthomason/packer.nvim" "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
-cd "$HOME/.dotfiles/.config/nvim" || exit
+stow -d .dotfiles .
 echo -e "\e[32m$1\e[0mInstalling Plugins"
-stow -d "$HOME/.dotfiles" "$HOME"
-/bin/nvim --headless -c 'source lua/tiagorg/packer.lua' -c 'PackerSync' -c 'qa!'
-cd "$OLDPWD" || exit
+/bin/nvim --headless -c 'source /home/tiagorg/.dotfiles/.config/nvim/lua/tiagorg/packer.lua' -c 'PackerSync' -c 'qa!'
 
 # Setup zsh
 echo -e "\e[32m$1\e[0mSetting up zsh"
@@ -40,4 +48,7 @@ echo -e "\e[32m$1\e[0mInstalling zsh-autosuggestions"
 git clone "https://github.com/zsh-users/zsh-autosuggestions.git" "$HOME/.dotfiles/.zsh/zsh-autosuggestions"
 echo -e "\e[32m$1\e[0mInstalling zsh-syntax-highlighting"
 git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$HOME/.dotfiles/.zsh/zsh-syntax-highlighting"
+
+# Restore the original directory
+cd "$ORIGIN_DIR" || exit
 
